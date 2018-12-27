1d00e5cbb06887589245d9c012a1b155fdf5d208
jason <342690199@qq.com>
2018-12-27 19:40:52 +0800

    update vuex with action & namespcaed

--- Project1/count/src/App.vue
+++ Project1/count/src/App.vue
@@ -14,9 +14,17 @@ export default {
   methods: {
     changeNum(type){
       // this.$store.commit('changeNum', {payload: type});
-      this.$store.commit({
-        type: 'changeNum',
+      // 触发mutation
+      // this.$store.commit({
+      //   type: 'app/changeNum',
+      //   payload: type
+      // })
+      // 触发action
+      this.$store.dispatch({
+        type: 'app/changeNumAsync',
         payload: type
+      }).then(res=>{
+        console.log('操作完成了！');
       })
     }
   }
--- Project1/count/src/store/index.js
+++ Project1/count/src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+// vuex日志插件，可以记录每一步mutation
+import createLogger from 'vuex/dist/logger';
 // 引入模块
 import app from './modules/app';
 Vue.use(Vuex);
@@ -8,5 +10,10 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     app
-  }
+  },
+  state: {
+    a: 100,
+    b: 200
+  },
+  plugins: [createLogger()]
 })
