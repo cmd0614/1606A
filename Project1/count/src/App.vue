b845903c1fd58cdba26117787d857406f83d9169
jason <342690199@qq.com>
2018-12-28 10:15:28 +0800

    add 辅助方法

--- Project1/count/src/App.vue
+++ Project1/count/src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
-    <button @click="changeNum('+')">+</button>
-    <span>{{this.$store.state.app.num}}</span>
-    <button @click="changeNum('-')">-</button>
+    <button @click="changeNum({payload: '+'})">+</button>
+    <span>{{num}}</span>
+    <span>日期：{{date}}</span>
+    <button @click="changeNum({payload: '-'})">-</button>
   </div>
 </template>
 
 <script>
+import {mapState, mapGetters, mapMutations, mapActions} from 'vuex';
 export default {
+  computed: {
+    ...mapState({
+      num: state=>state.app.num
+    }),
+    ...mapGetters({
+      date: 'app/date'
+    })
+  },
   mounted(){
     console.log(this.$store);
   },
   methods: {
+    ...mapMutations({
+      changeNumSync: 'app/changeNum'
+    }),
+    ...mapActions({
+      changeNumAsync: 'app/changeNumAsync'
+    }),
     changeNum(type){
       // this.$store.commit('changeNum', {payload: type});
       // 触发mutation
@@ -20,10 +36,14 @@ export default {
       //   payload: type
       // })
       // 触发action
-      this.$store.dispatch({
-        type: 'app/changeNumAsync',
-        payload: type
-      }).then(res=>{
+      // this.$store.dispatch({
+      //   type: 'app/changeNumAsync',
+      //   payload: type
+      // }).then(res=>{
+      //   console.log('操作完成了！');
+      // })
+      this.changeNumAsync({payload: type})
+      .then(res=>{
         console.log('操作完成了！');
       })
     }
--- Project1/count/src/store/modules/app.js
+++ Project1/count/src/store/modules/app.js
@@ -14,7 +14,7 @@ const getters = {
         hour = date.getHours().toString().padStart(2, '0'),
         min = date.getMinutes().toString().padStart(2, '0'),
         sec = date.getSeconds().toString().padStart(2, '0');
-    return `${year}-${month}-${day} ${hour}:${min}:${sec}`;
+    return `${year}-${month}-${day} ${hour}:${min}:${sec} - ${state.num}`;
   }
 }
 
