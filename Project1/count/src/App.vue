a00748dfddd1b6982dc4936ad978229e196d0868
jason <342690199@qq.com>
2018-12-29 11:14:28 +0800

    update JSBridge

--- Project1/count/src/App.vue
+++ Project1/count/src/App.vue
@@ -29,19 +29,6 @@ export default {
       changeNumAsync: 'app/changeNumAsync'
     }),
     changeNum(type){
-      // this.$store.commit('changeNum', {payload: type});
-      // 触发mutation
-      // this.$store.commit({
-      //   type: 'app/changeNum',
-      //   payload: type
-      // })
-      // 触发action
-      // this.$store.dispatch({
-      //   type: 'app/changeNumAsync',
-      //   payload: type
-      // }).then(res=>{
-      //   console.log('操作完成了！');
-      // })
       this.changeNumAsync({payload: type})
       .then(res=>{
         console.log('操作完成了！');
