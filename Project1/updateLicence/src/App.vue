a93020c09e709e123d8dc9bf84ea32e6deff9982
jason <342690199@qq.com>
2019-01-02 16:01:23 +0800

    更新项目结构

--- Project1/updateLicence/src/App.vue
+++ Project1/updateLicence/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <button @click="login">点击登陆</button>
+  <!-- <button @click="login">点击登陆</button> -->
+  <router-view></router-view>
 </template>
 
 <script>
@@ -22,6 +23,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import './scss/common.scss';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ Project1/updateLicence/src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>我是Index</h1>
+    <Upload/>
+  </div>
+</template>
+
+<script>
+import Upload from '@/components/Upload';
+export default {
+  components: {
+    Upload
+  }
+}
+</script>
